5e83577cad7330190fe786134391a586f54c147b
Brant Keener <brant_keener@yahoo.com>
2019-04-07 11:03:37 -0600

    Add some HTML

--- server.js
+++ server.js
@@ -5,14 +5,24 @@ const mysql = require('mysql');
 // All the pertinent information to contact our DB
 const sqlDBConnection = mysql.createConnection({
     host: 'localhost',
-    port: 3306,
+    port: process.env.PORT,
     user: 'root',
     password: process.env.SUBLIST_PASS,
     database: 'sublist_sortDB'
 });
 
+// Function pulls and displays data;
+const pullAllDBData = () => {
+  const query = "SELECT * FROM sublist_sort_items"
+  sqlDBConnection.query(query, (err, res) => {
+    if(err) throw err;
+    console.log(res);
+  });
+};
+
 // Begin the connection, and fire a function to load the list of items for sale.
 sqlDBConnection.connect((err) => {
     if(err) throw err;
-    console.log(`\nConnected on ${port}\n`)
+    console.log(`\nConnected to ${process.env.PORT}\n`)
+    pullAllDBData();
 }); 
